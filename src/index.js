5289328d5dd77e6f43be8ed605ded4fe42f4047d
Axel Niklasson <axel.niklasson@live.com>
2018-06-03 23:32:36 +0200

    Restructure prior to eject

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'containers/App';
-import { BrowserRouter } from 'react-router-dom'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import App from './App';
 
-ReactDOM.render((
-  <MuiThemeProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </MuiThemeProvider>
-), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
